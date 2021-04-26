0f3ba2f5a44bce2bceab80b7cb17ce01e2d7bd55
ismabarroso <ismaelbarroso2001@ginebro.cat>
2021-04-26 18:31:52 +0200

    TemperaturaDocNoComent

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"temps"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Temperatura"}]
